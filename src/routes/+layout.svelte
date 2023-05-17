9a11b8108637629bd078988a83457b76e5c265c4
Frecherenkel60 <rieger.kevin01@gmail.com>
2023-05-17 10:23:03 +0200

    feat: implement expandable menu bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
 	import TopAppBar, { Row, Section, Title, AutoAdjust } from '@smui/top-app-bar';
+	import Drawer, {
+		AppContent,
+		Content,
+		Header,
+		Title as DrawerTitle,
+		Subtitle
+	} from '@smui/drawer';
+	import List, { Item, Graphic, Separator, Subheader, Text } from '@smui/list';
 	import IconButton from '@smui/icon-button';
 
 	let topAppBar: TopAppBar;
+	let drawerOpened = false;
+	$: menuIcon = drawerOpened ? 'menu_open' : 'menu';
+
+	// Theme related variables and functions
 	let lightTheme =
 		typeof window === 'undefined' || window.matchMedia('(prefers-color-scheme: light)').matches;
-
 	$: themeIcon = lightTheme ? 'light_mode' : 'dark_mode';
 	$: themeLabel = lightTheme ? 'Switch to dark theme' : 'Switch to light theme';
 
@@ -28,7 +39,11 @@
 <TopAppBar bind:this={topAppBar} variant="standard" class="navbar">
 	<Row>
 		<Section>
-			<IconButton class="material-icons">menu</IconButton>
+			<IconButton
+				class="material-icons"
+				aria-label="menu_icon"
+				on:click={() => (drawerOpened = !drawerOpened)}>{menuIcon}</IconButton
+			>
 			<Title>Expense Management</Title>
 		</Section>
 		<Section align="end" toolbar>
@@ -40,8 +55,61 @@
 		</Section>
 	</Row>
 </TopAppBar>
-<AutoAdjust {topAppBar} class="auto-adjust">
-	<div class="container">
-		<slot />
+
+<AutoAdjust {topAppBar}>
+	<div class="drawer-container">
+		<Drawer variant="dismissible" bind:open={drawerOpened}>
+			<Header>
+				<DrawerTitle>Travel Utilities</DrawerTitle>
+				<Subtitle>Expense Management</Subtitle>
+			</Header>
+			<Content>
+				<Separator />
+				<Subheader tag="h6">Allgemein</Subheader>
+				<List>
+					<Item href="/cost" on:click={() => (drawerOpened = !drawerOpened)}>
+						<Graphic class="material-icons">payments</Graphic>
+						<Text>Cost-Overview</Text>
+					</Item>
+					<Item href="/trip" on:click={() => (drawerOpened = !drawerOpened)}>
+						<Graphic class="material-icons">connecting_airports</Graphic>
+						<Text>Trip-Overview</Text>
+					</Item>
+				</List>
+				<Separator />
+				<Subheader tag="h6">Sonstiges</Subheader>
+			</Content>
+		</Drawer>
+
+		<AppContent>
+			<div class="main-content">
+				<slot />
+			</div>
+		</AppContent>
 	</div>
 </AutoAdjust>
+```
+
+<style>
+	.drawer-container {
+		position: relative;
+		display: flex;
+		border: 1px solid var(--mdc-theme-text-hint-on-background, rgba(0, 0, 0, 0.1));
+		overflow: hidden;
+		z-index: 0;
+		height: 100vh;
+	}
+
+	* :global(.app-content) {
+		flex: auto;
+		overflow: auto;
+		position: relative;
+		flex-grow: 1;
+	}
+
+	.main-content {
+		overflow: auto;
+		padding: 16px;
+		box-sizing: border-box;
+	}
+</style>
