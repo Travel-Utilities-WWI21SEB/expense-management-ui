4ad8899de9e01dbfe0105f3c8919abfa00ff869d
Aidan Zimmer <aidanmichael@web.de>
2023-05-21 19:03:25 +0200

    fix: fix lint with format

--- src/interfaces/TripOverview.ts
+++ src/interfaces/TripOverview.ts
@@ -1,26 +1,24 @@
 export interface ChartData {
-    labels: Array<string>,
-    datasets: Array<
-        {
-            data: Array<number>,
-            backgroundColor: Array<string>
-        }
-    >
+	labels: Array<string>;
+	datasets: Array<{
+		data: Array<number>;
+		backgroundColor: Array<string>;
+	}>;
 }
 
 export interface TravelData {
-    id: string
-    name: string,
-    costcategories: Array<Costcategories>,
-    data: ChartData | undefined,
-    startDate: Date,
-    endDate: Date,
-    totalCost: number | undefined,
-    location: string
+	id: string;
+	name: string;
+	costcategories: Array<Costcategories>;
+	data: ChartData | undefined;
+	startDate: Date;
+	endDate: Date;
+	totalCost: number | undefined;
+	location: string;
 }
 
 export interface Costcategories {
-    name: string,
-    amount: number,
-    color: string
+	name: string;
+	amount: number;
+	color: string;
 }
