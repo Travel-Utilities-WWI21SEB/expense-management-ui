5fe88541b733b4369569cecf87ae0db78e58b7ef
johannadke <johanna.dke@web.de>
2023-05-27 19:31:51 +0200

    feat: select cost item

--- src/components/tripDetails/_CostItemTripDetails.svelte
+++ src/components/tripDetails/_CostItemTripDetails.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
+	import { Avatar } from '@skeletonlabs/skeleton';
 	import { createEventDispatcher } from 'svelte';
 	import type { Cost } from '$tripDomain';
-	import { calculateDate } from '$utils';
 
 	export let cost: Cost;
 	export let i: number;
@@ -14,49 +14,27 @@
 			index: i
 		});
 	}
-
-	function pickTextColorBasedOnBgColorSimple(
-		bgColor: string,
-		lightColor: string,
-		darkColor: string
-	): string {
-		var color = bgColor.charAt(0) === '#' ? bgColor.substring(1, 7) : bgColor;
-		var r = parseInt(color.substring(0, 2), 16); // hexToR
-		var g = parseInt(color.substring(2, 4), 16); // hexToG
-		var b = parseInt(color.substring(4, 6), 16); // hexToB
-		return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? darkColor : lightColor;
-	}
 </script>
 
 <button
-	class="card card-hover hover:bg-secondary-100 hover:dark:text-secondary-900 w-full {selectionIndex ===
-	i
-		? 'bg-secondary-200  outline outline-1'
+	class="card card-hover hover:bg-orange-100 focus:bg-orange-100 w-full {selectionIndex === i
+		? 'bg-orange-200'
 		: ''}"
 	on:click={() => selectListItem(i)}
 >
 	<div class="grid grid-cols-12 md:gap-2">
-		<div class="col-span-12 sm:col-span-2 grid content-center p-2">
-			<div class="text-clip overflow-hidden content-center">
-				<p
-					class="w-full text-sm text-slate-500 mr-4 py-2 px-4
-					 rounded-full border-0 text-sm font-semibold
-					 text-[{pickTextColorBasedOnBgColorSimple(cost.costCategory.color, '#FFFFFF', '#000000')}]
-					  bg-[{cost.costCategory.color}]"
-				>
-					{cost.costCategory.name}
-				</p>
+		<div class="col-start-1 col-span-2 grid content-center">
+			<div class="flex flex-row flex-wrap content-center">
+				<Avatar initials={cost.costCategory.name} background={cost.costCategory.color} />
 			</div>
 		</div>
-		<div class="col-span-8 sm:col-span-8 grid content-center p-2">
-			<div class="text-clip overflow-hidden text-left">
+		<div class="col-span-8 grid content-center">
+			<div class="flex flex-row flex-wrap overflow-hidden">
 				{cost.name}
-				<br />
-				{`${calculateDate(cost.startDate)} - ${calculateDate(cost.endDate)}`}
 			</div>
 		</div>
-		<div class="col-span-4 sm:col-span-2 grid content-center p-2">
-			<div class="text-clip font-semibold overflow-hidden w-full text-rigth text-error-500">
+		<div class="col-span-2 grid content-center">
+			<div class="flex flex-row flex-wrap w-full">
 				{cost.amount + ' ' + cost.currency}
 			</div>
 		</div>
