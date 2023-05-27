8900e6ffd059dc7f4e184b4cc1cb3368185a4547
zimmerai <aidanmichael@web.de>
2023-05-27 17:01:14 +0200

    refactor: remove unnecessary file

--- src/components/tripDetails/_ParticipantIcon.svelte
+++ src/components/tripDetails/_ParticipantIcon.svelte
@@ -1,17 +1,14 @@
 <script lang="ts">
 	import type { User } from '$userDomain';
 	import { Avatar } from '@skeletonlabs/skeleton';
-	import { Tooltip } from '$components';
 
 	export let participant: User;
 </script>
 
-<Tooltip>
-	<Avatar
-		initials={participant.name
-			.split(' ')
-			.map((val) => val.substring(0, 1))
-			.join('')}
-		background="bg-primary-500"
-	/>
-</Tooltip>
+<Avatar
+	initials={participant.name
+		.split(' ')
+		.map((val) => val.substring(0, 1))
+		.join('')}
+	background="bg-primary-500"
+/>
