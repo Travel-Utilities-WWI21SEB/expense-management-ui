c5aef464f6c3560198d520891df8dc32f174d64d
johannadke <johanna.dke@web.de>
2023-05-27 19:31:51 +0200

    feat: select cost item

--- src/components/tripDetails/_CostItemTripDetails.svelte
+++ src/components/tripDetails/_CostItemTripDetails.svelte
@@ -1,3 +1,46 @@
+<script lang="ts">
+	import { Avatar } from '@skeletonlabs/skeleton';
+	import { createEventDispatcher } from 'svelte';
+	import type { Cost } from '$tripDomain';
+
+	export let cost: Cost;
+	export let i: number;
+	export let selectionIndex: number;
+
+	const dispatch = createEventDispatcher();
+
+	function selectListItem(i: number) {
+		dispatch('select_item', {
+			index: i
+		});
+	}
+</script>
+
+<button
+	class="card card-hover hover:bg-orange-100 focus:bg-orange-100 w-full {selectionIndex === i
+		? 'bg-orange-200'
+		: ''}"
+	on:click={() => selectListItem(i)}
+>
+	<div class="grid grid-cols-12 md:gap-2">
+		<div class="col-start-1 col-span-2 grid content-center">
+			<div class="flex flex-row flex-wrap content-center">
+				<Avatar initials={cost.costCategory.name} background={cost.costCategory.color} />
+			</div>
+		</div>
+		<div class="col-span-8 grid content-center">
+			<div class="flex flex-row flex-wrap overflow-hidden">
+				{cost.name}
+			</div>
+		</div>
+		<div class="col-span-2 grid content-center">
+			<div class="flex flex-row flex-wrap w-full">
+				{cost.amount + ' ' + cost.currency}
+			</div>
+		</div>
+	</div>
+</button>
+
 <!-- <script lang="ts">
 	import { Graphic, Item, Meta, PrimaryText, SecondaryText, Text } from '@smui/list';
 	import type { Cost } from '$tripDomain';
--- src/components/tripDetails/_CostsOverviewTripDetails.svelte
+++ src/components/tripDetails/_CostsOverviewTripDetails.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { TripDetailsCostItem } from '$components';
 	import type { Cost } from '$tripDomain';
 	import { Avatar } from '@skeletonlabs/skeleton';
 
@@ -16,25 +17,12 @@
 	<ul class="list p-4">
 		{#each costs as cost, i}
 			<li>
-				<div class="card card-hover w-full">
-					<div class="grid grid-cols-12 md:gap-2">
-						<div class="col-start-1 col-span-2 grid content-center">
-							<div class="flex flex-row flex-wrap content-center">
-								<Avatar initials={cost.costCategory.name} background={cost.costCategory.color} />
-							</div>
-						</div>
-						<div class="col-span-8 grid content-center">
-							<div class="flex flex-row flex-wrap">
-								{cost.name}
-							</div>
-						</div>
-						<div class="col-span-2 grid content-center">
-							<div class="flex flex-row flex-wrap w-full">
-								{cost.amount + ' ' + cost.currency}
-							</div>
-						</div>
-					</div>
-				</div>
+				<TripDetailsCostItem
+					{cost}
+					{selectionIndex}
+					{i}
+					on:select_item={(e) => handleSelectItem(e)}
+				/>
 			</li>
 		{/each}
 	</ul>
