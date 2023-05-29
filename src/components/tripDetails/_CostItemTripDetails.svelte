b5a411c3fd2e8136cae267c783ea1f97a0f905b5
johannadke <johanna.dke@web.de>
2023-05-29 11:58:18 +0200

    style: succes and error colors and feat: time of cost

--- src/components/tripDetails/_CostItemTripDetails.svelte
+++ src/components/tripDetails/_CostItemTripDetails.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import { Avatar } from '@skeletonlabs/skeleton';
 	import { createEventDispatcher } from 'svelte';
 	import type { Cost } from '$tripDomain';
+	import { calculateDate } from '$utils';
 
 	export let cost: Cost;
 	export let i: number;
@@ -51,59 +51,14 @@
 		<div class="col-span-8 sm:col-span-8 grid content-center p-2">
 			<div class="text-clip overflow-hidden text-left">
 				{cost.name}
+				<br />
+				{`${calculateDate(cost.startDate)} - ${calculateDate(cost.endDate)}`}
 			</div>
 		</div>
 		<div class="col-span-4 sm:col-span-2 grid content-center p-2">
-			<div class="text-clip overflow-hidden w-full text-rigth">
+			<div class="text-clip font-semibold overflow-hidden w-full text-rigth text-error-500">
 				{cost.amount + ' ' + cost.currency}
 			</div>
 		</div>
 	</div>
 </button>
-
-<!-- <script lang="ts">
-	import { Graphic, Item, Meta, PrimaryText, SecondaryText, Text } from '@smui/list';
-	import type { Cost } from '$tripDomain';
-	import { createEventDispatcher } from 'svelte';
-	import { calculateDate } from '$utils';
-
-	export let cost: Cost;
-	export let i: number;
-	export let selectionIndex: number;
-
-	const dispatch = createEventDispatcher();
-
-	function selectListItem(i: number) {
-		dispatch('select_item', {
-			index: i
-		});
-	}
-</script>
-
-<Item on:SMUI:action={() => selectListItem(i)} selected={selectionIndex === i}>
-	<Graphic
-		style="background-image: url(https://placehold.co/40x40/${cost.costCategory
-			.color}/ffffff?text={cost.costCategory.name
-			.split(' ')
-			.map((val) => val.substring(0, 1))
-			.join('')});"
-	/>
-	<Text>
-		<PrimaryText>{cost.name}</PrimaryText>
-		<SecondaryText
-			>{`${calculateDate(cost.startDate)} - ${calculateDate(cost.endDate)}`}</SecondaryText
-		>
-	</Text>
-	<Meta class="demo-list">
-		<div class="mdc-typography--headline6">
-			{`${cost.amount.toString()} ${cost.currency}`}
-		</div>
-	</Meta>
-</Item>
-
-<style>
-	* :global(.demo-list) {
-		max-width: 600px;
-	}
-</style>
- -->
