aaaa651fd180cd41f5fbb33055d44b54fdd68b7c
Lisa Ress-Park <s212547@student.dhbw-mannheim.de>
2023-06-02 15:30:14 +0200

    feat: fix carousel layout

--- src/components/homescreen/_TripCarousel.svelte
+++ src/components/homescreen/_TripCarousel.svelte
@@ -1,117 +1,107 @@
 <script lang="ts">
-	import type { TravelData } from "$tripDomain";
-	import { TripCard } from "$components";
+	import type { TravelData } from '$tripDomain';
+	import { TripCard } from '$components';
+	import { goto } from '$app/navigation';
 
-  let duration = 3000; 
-  export let trips: Array<TravelData>
-  $: current = 0;
-  setInterval(() => {
-    if (current == trips.length - 1) {
-      current = 0;
-    } else {
-      current++;
-    }
-  }, duration);
+	let duration = 5000;
+	export let trips: Array<TravelData>;
+	$: currentTrip = 0;
+	let timer = setInterval(() => setInitialInterval(), duration);
+	function setInitialInterval() {
+		if (currentTrip == trips.length - 1) {
+			currentTrip = 0;
+		} else {
+			currentTrip++;
+		}
+	}
+	function slideCarousel(direction: string) {
+		let buttonDirection = direction === 'left' ? 0 : trips.length - 1;
+		if (currentTrip == buttonDirection) {
+			currentTrip = direction === 'left' ? trips.length - 1 : 0;
+		} else {
+			direction === 'left' ? currentTrip-- : currentTrip++;
+		}
+		clearInterval(timer);
+		timer = setInterval(() => setInitialInterval(), duration);
+	}
+
+	function onTripCardClick() {
+		goto(`/trips`);
+	}
 </script>
 
-<div class="carousel">
-  <!-- Carousel wrapper -->
-  <div class="wrapper">
-    {#each trips as trip, index}
-      <div class="item card h-64 {current == index ? 'z-20' : ''}" data-value={index}>
-        <TripCard {trip}/>
-      </div>
-    {/each}
-  </div>
-  <!-- Slider indicators -->
-  <div class="indicators">
-    {#each trips as trip, index}
-      <button
-        on:click={() => {
-          current = index;
-        }}
-        type="button"
-        aria-current={current == index ? true : false}
-        aria-label={trip.name}
-        data-value={index}
-      />
-    {/each}
-  </div>
-  <!-- Slider controls -->
-  <button
-    on:click={() => {
-      if (current == 0) {
-        current = trips.length - 1;
-      } else {
-        current--;
-      }
-    }}
-    type="button"
-    class="controls top-0 left-0"
-    data-carousel-prev=""
-  >
-    <span class="item">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-</svg>
-      <span class="hidden">Previous</span>
-    </span>
-  </button>
-  <button
-    on:click={() => {
-      if (current == trips.length - 1) {
-        current = 0;
-      } else {
-        current++;
-      }
-    }}
-    type="button"
-    class="controls top-0 right-0"
-    data-carousel-next=""
-  >
-    <span class="item">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-</svg>
-      <span class="hidden">Next</span>
-    </span>
-  </button>
+<div class="carousel relative">
+	<!-- Carousel wrapper -->
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<div class="wrapper relative card-hover h-96 rounded-lg" on:click={() => onTripCardClick()}>
+		{#each trips as trip, index}
+			<div class="item duration-700 ease-in-out absolute inset-0 transition-all transform translate-x-0 card h-fit p-4 {currentTrip == index ? 'z-20' : ''}" data-value={index}>
+				<TripCard {trip} />
+			</div>
+		{/each}
+		</div>
+	<!-- Slider indicators -->
+	<div class="indicators flex absolute bottom-5 left-1/2 z-30 space-x-3 -translate-x-1/2">
+		{#each trips as trip, index}
+			<button
+				on:click={() => {
+					currentTrip = index;
+				}}
+				type="button"
+				aria-current={currentTrip == index ? true : false}
+				aria-label={trip.name}
+				data-value={index}
+			/>
+		{/each}
+	</div>
+	<!-- Slider controls -->
+	<button
+		on:click={() => slideCarousel('left')}
+		type="button"
+		class="controls top-0 left-0 flex absolute z-30 justify-center items-center px-4 h-full cursor-pointer focus:outline-none"
+		data-carousel-prev=""
+	>
+		<span class="item">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-6 h-6 w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-100"
+			>
+				<path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+			</svg>
+			<span class="hidden">Previous</span>
+		</span>
+	</button>
+	<button
+		on:click={() => slideCarousel('right')}
+		type="button"
+		class="controls top-0 right-0 absolute z-30 justify-center items-center px-4 h-full cursor-pointer focus:outline-none"
+		data-carousel-next=""
+	>
+		<span class="item inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-6 h-6"
+			>
+				<path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+			</svg>
+			<span class="hidden">Next</span>
+		</span>
+	</button>
 </div>
 
 <style>
-    
-  .carousel {
-    @apply relative;
-  }
-  .carousel .wrapper {
-    @apply overflow-hidden relative h-96 rounded-lg;
-  }
-  .carousel .wrapper .item {
-    @apply duration-700 ease-in-out absolute inset-0 transition-all transform translate-x-0;
-  }
-  .carousel .wrapper .item img {
-    @apply block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2;
-  }
-  .carousel .wrapper .item span {
-    @apply absolute top-1/2 left-1/2 text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 sm:text-3xl dark:text-gray-800;
-  }
-  .carousel .indicators {
-    @apply flex absolute bottom-5 left-1/2 z-30 space-x-3 -translate-x-1/2;
-  }
-  .carousel .indicators [aria-current="true"] {
-    @apply w-3 h-3 rounded-full bg-white dark:bg-gray-800;
-  }
-  .carousel .indicators [aria-current="false"] {
-    @apply w-3 h-3 rounded-full bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800;
-  }
-  .carousel .controls {
-    @apply flex absolute z-30 justify-center items-center px-4 h-full cursor-pointer focus:outline-none;
-  }
-  .carousel .controls .item {
-    @apply inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30;
-  }
-  .carousel .controls .item svg {
-    @apply w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-100; 
-  }
-
+	.carousel .indicators [aria-current='true'] {
+		@apply w-3 h-3 rounded-full bg-white dark:bg-gray-800;
+	}
+	.carousel .indicators [aria-current='false'] {
+		@apply w-3 h-3 rounded-full bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800;
+	}
 </style>
--- src/components/tripOverview/_TripCard.svelte
+++ src/components/tripOverview/_TripCard.svelte
@@ -5,21 +5,22 @@
 	export let trip: TravelData;
 </script>
 
-<h3 class="h3 justify-center">
-	{trip.name}
-</h3>
-<div class="md:grid md:grid-cols-2">
-	<div class="flex flex-col justify-center">
-		<TripInfos {trip} />
-		<UserPaymentOverview {trip} />
-		<Participants participants={trip.participants} justifyCenter={true} />
-	</div>
-	{#if trip.data && trip.totalCost}
+	<h3 class="h3 justify-center">
+		{trip.name}
+	</h3>
+	<div class="md:grid md:grid-cols-2">
+		<div class="flex flex-col justify-center">
+			<TripInfos {trip} />
+			<UserPaymentOverview {trip} />
+			<Participants participants={trip.participants} justifyCenter={true} />
+		</div>
+		{#if trip.data && trip.totalCost}
 		<div class="pr-4 pb-4">
 			<h6 class="h6 p-2 flex justify-center">
 				Total cost: {trip.totalCost}€
 			</h6>
 			<DonutChart data={trip.data} />
 		</div>
-	{/if}
-</div>
+		{/if}
+	</div>
+	
