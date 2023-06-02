2b813d6932aaee65c54d836322230b6341e00821
johannadke <johanna.dke@web.de>
2023-06-02 13:49:04 +0200

    feat: scrollbar and some style

--- src/components/general/_HeaderAndTwoPartsLayout.svelte
+++ src/components/general/_HeaderAndTwoPartsLayout.svelte
@@ -1,20 +1,11 @@
-<script>
-</script>
-
-<div class="element py-4 px-4">
+<div class="px-4 py-2">
 	<slot name="header" />
 </div>
 <div class="xl:grid xl:grid-cols-2 px-4 place-items-stretch">
-	<div class="element py-4">
+	<div class="element xl:pr-2 pb-4">
 		<slot name="left_element" />
 	</div>
-	<div class="element py-4 content-start">
+	<div class="element xl:pl-2 pb-4 content-start">
 		<slot name="right_element" />
 	</div>
 </div>
-
-<style>
-	.element {
-		width: 98%;
-	}
-</style>
