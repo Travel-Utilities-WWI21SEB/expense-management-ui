291eda092752808c965009ee15fe814a3427cb40
zimmerai <aidanmichael@web.de>
2023-06-02 16:34:17 +0200

    feat: begin first implemenation of create modal

--- src/utils/trips/modifyTripData.ts
+++ src/utils/trips/modifyTripData.ts
@@ -12,14 +12,21 @@ export function modifyTripData(tripData: Array<TravelData>) {
 				}
 			]
 		};
-		trip.costCategories.map((category) => {
-			tripChartData.labels.push(category.name);
-			tripChartData.datasets[0].data.push(category.totalAmount);
-			tripChartData.datasets[0].backgroundColor.push(category.color);
-			return category;
-		});
-		trip.data = tripChartData;
-		trip.totalCost = tripChartData.datasets[0].data.reduce((partialSum, a) => partialSum + a, 0);
+		if (trip.costCategories.length > 1) {
+			trip.costCategories.map((category) => {
+				tripChartData.labels.push(category.name);
+				tripChartData.datasets[0].data.push(category.totalAmount);
+				tripChartData.datasets[0].backgroundColor.push(category.color);
+				return category;
+			});
+			trip.data = tripChartData;
+			trip.totalCost = tripChartData.datasets[0].data.reduce((partialSum, a) => partialSum + a, 0);
+		} else {
+			trip.data = {
+				labels: ['no cost specified yet'],
+				datasets: [{ data: [-1], backgroundColor: ['#808080'] }]
+			};
+		}
 
 		return trip;
 	});
