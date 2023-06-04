d404e74603f7f531c213933c97e35a30719ed760
johannadke <johanna.dke@web.de>
2023-06-04 08:27:21 +0200

    feat: first draft of editing costs

--- src/components/tripDetails/Modal/_ShowCostItemTripDetailsModal.svelte
+++ src/components/tripDetails/Modal/_ShowCostItemTripDetailsModal.svelte
@@ -1,13 +1,26 @@
 <script lang="ts">
-	import type { Cost } from '$tripDomain';
-	import { calculateDate } from '$utils';
+	import { calculateDate, pickTextColorBasedOnBgColorSimple } from '$utils';
+	import type { CostDateAsString } from '$tripDomain';
 
-	export let cost: Cost;
+	export let cost: CostDateAsString;
 </script>
 
 <h1 class="h1">{cost.name}</h1>
-<h3 class="h3">{calculateDate(cost.startDate)} - {calculateDate(cost.endDate)}</h3>
+<h3 class="h3">
+	{calculateDate(new Date(cost.startDate))}
+	{#if cost.endDate}
+		- {calculateDate(new Date(cost.endDate))}
+	{/if}
+</h3>
 <div class="text-clip font-semibold overflow-hidden w-full text-rigth text-error-500">
 	{cost.amount + ' ' + cost.currency}
 </div>
-<span class="badge bg-[{cost.costCategory.color}]">{cost.costCategory.name}</span>
+<p
+	class="text-sm py-2 mr-2 px-4 rounded-full border-2 font-semibold text-[{pickTextColorBasedOnBgColorSimple(
+		cost.costCategory.color,
+		'#FFFFFF',
+		'#000000'
+	)}] border-[{cost.costCategory.color}]"
+>
+	{cost.costCategory.name}
+</p>
