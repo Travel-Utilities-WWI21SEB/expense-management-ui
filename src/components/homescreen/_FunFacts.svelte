989a9b16f866376478cb24f28fb2cab2a1db4682
Lisa Ress-Park <s212547@student.dhbw-mannheim.de>
2023-06-25 22:26:03 +0200

    little changes to fun facts on homescreen

--- src/components/homescreen/_FunFacts.svelte
+++ src/components/homescreen/_FunFacts.svelte
@@ -17,50 +17,50 @@
             <h4 class="h4 flex justify-center p-2">People you travel often with</h4>
             <ul class="list place-content-stretch">
                 <li>
-                    <span>(icon)</span>#
+                    <span>(icon)</span>
                     <span class="flex-auto">Skeleton</span>
                 </li>
                 <li>
-                    <span>(icon)</span>#
+                    <span>(icon)</span>
                     <span class="flex-auto">Skeleton</span>
                 </li>
                 <li>
-                    <span>(icon)</span>#
-                    <span class="flex-auto">Skeleton</span>
-                </li>
-            </ul>
-        </div>
-        <div class="card card-hover hover:bg-secondary-100 hover:dark:text-secondary-900 w-full">
-            <h4 class="h4 flex justify-center p-2">People you travel often with</h4>
-            <ul class="list place-content-stretch">
-                <li>
-                    <span>(icon)</span>#
-                    <span class="flex-auto">Skeleton</span>
-                </li>
-                <li>
-                    <span>(icon)</span>#
-                    <span class="flex-auto">Skeleton</span>
-                </li>
-                <li>
-                    <span>(icon)</span>#
+                    <span>(icon)</span>
                     <span class="flex-auto">Skeleton</span>
                 </li>
             </ul>
         </div>
         <div class="card card-hover hover:bg-secondary-100 hover:dark:text-secondary-900 w-full">
-            <h4 class="h4 flex justify-center p-2">People you travel often with</h4>
+            <h4 class="h4 flex justify-center p-2">Most Expensive Categories</h4>
             <ul class="list place-content-stretch">
                 <li>
-                    <span>(icon)</span>#
-                    <span class="flex-auto">Skeleton</span>
+                    <span>(icon)</span>
+                    <span class="flex-auto">Miete</span>
                 </li>
                 <li>
-                    <span>(icon)</span>#
-                    <span class="flex-auto">Skeleton</span>
+                    <span>(icon)</span>
+                    <span class="flex-auto">Essen</span>
                 </li>
                 <li>
-                    <span>(icon)</span>#
-                    <span class="flex-auto">Skeleton</span>
+                    <span>(icon)</span>
+                    <span class="flex-auto">Fun</span>
+                </li>
+            </ul>
+        </div>
+        <div class="card card-hover hover:bg-secondary-100 hover:dark:text-secondary-900 w-full">
+            <h4 class="h4 flex justify-center p-2">Current Travel Destinations</h4>
+            <ul class="list place-content-stretch">
+                <li>
+                    <span>(icon)</span>
+                    <span class="flex-auto">Palo Alto</span>
+                </li>
+                <li>
+                    <span>(icon)</span>
+                    <span class="flex-auto">New Zealand</span>
+                </li>
+                <li>
+                    <span>(icon)</span>
+                    <span class="flex-auto">Canada</span>
                 </li>
             </ul>
         </div>
