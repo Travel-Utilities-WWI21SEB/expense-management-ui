20570c08edf98b1f1030a3df25467acbf2fcd523
Lisa Ress-Park <s212547@student.dhbw-mannheim.de>
2023-06-29 09:15:38 +0200

    feat: homescreen change in headline

--- src/routes/(app)/home/+page.svelte
+++ src/routes/(app)/home/+page.svelte
@@ -40,7 +40,7 @@
 	<HeaderAndTwoPartsLayout>
 		<span slot="header">
 			<div class="mdc-elevation--z3">
-				<h2 class="h2 text-center mt-2 text-primary-800 dark:text-primary-400">Everything at glance</h2>
+				<h2 class="h2 text-center mt-2 bg-gradient-to-br from-primary-800 to-primary-400 bg-clip-text text-transparent box-decoration-clone">Everything at glance</h2>
 			</div>
 		</span>
 		<span slot="left_element">
